09c2950bef4e8cc2b924cd0f5a9de1b483392283
Priyanshu Kumar <priyanshukr7439@gmail.com>
2026-04-26 13:22:32 +0530

    made API dynamic

--- api/routes.py
+++ api/routes.py
@@ -13,10 +13,21 @@ wind_model = joblib.load(ARTIFACT_PATH / "wind_model.pkl")
 
 
 @router.get("/predict")
-def predict_energy():
+@router.get("/predict")
+def predict_energy(
+    irradiation: float,
+    temperature: float,
+    module: float,
+    hour: int,
+    day: int,
+    month: int,
+    wind_speed: float,
+    direction: float,
+    theoretical: float
+):
 
-    solar_features = np.array([[800, 32, 40, 12, 15, 6]])
-    wind_features = np.array([[6.5, 250, 700]])
+    solar_features = np.array([[irradiation, temperature, module, hour, day, month]])
+    wind_features = np.array([[wind_speed, direction, theoretical]])
 
     solar = float(solar_model.predict(solar_features)[0])
     wind = float(wind_model.predict(wind_features)[0])
